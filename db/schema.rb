6060f0dea50c0ce4c49eb7d253bf6b038daefacf
Vitalii Paprotskyi <vetalpaprotsky@gmail.com>
2024-01-02 18:33:46 +0200

    adds quotes

--- db/schema.rb
+++ db/schema.rb
@@ -10,5 +10,11 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 0) do
+ActiveRecord::Schema[7.1].define(version: 2024_01_02_160858) do
+  create_table "quotes", charset: "utf8mb4", force: :cascade do |t|
+    t.string "name", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
 end
