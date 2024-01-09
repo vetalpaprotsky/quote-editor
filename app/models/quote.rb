8ebc06934cd880293e6adfdeb591628f03e6945b
Vitalii Paprotskyi <vetalpaprotsky@gmail.com>
2024-01-09 20:40:42 +0200

    adds users and companies

--- app/models/quote.rb
+++ app/models/quote.rb
@@ -1,16 +1,18 @@
 class Quote < ApplicationRecord
+  belongs_to :company
+
   validates :name, presence: true
 
   scope :ordered, -> { order(id: :desc) }
 
-  # after_create_commit -> { broadcast_prepend_later_to "quotes", partial: "quotes/quote", locals: { quote: self }, target: "quotes" }
+  # after_create_commit -> { broadcast_prepend_later_to [company, "quotes"], partial: "quotes/quote", locals: { quote: self }, target: "quotes" }
   # Equivalent to the above:
-  after_create_commit -> { broadcast_prepend_later_to "quotes" }
-  # after_update_commit -> { broadcast_replace_later_to "quotes", partial: "quotes/quote", locals: { quote: self }, target: "quote_#{id}" }
+  after_create_commit -> { broadcast_prepend_later_to [company, "quotes"] }
+  # after_update_commit -> { broadcast_replace_later_to [company, "quotes"], partial: "quotes/quote", locals: { quote: self }, target: "quote_#{id}" }
   # Equivalent to the above:
-  after_update_commit -> { broadcast_replace_later_to "quotes" }
-  after_destroy_commit -> { broadcast_remove_to "quotes" }
+  after_update_commit -> { broadcast_replace_later_to [company, "quotes"] }
+  after_destroy_commit -> { broadcast_remove_to [company, "quotes"] }
 
   # Above 3 callbacks can be replaced by this method.
-  # broadcasts_to ->(quote) { "quotes" }, inserts_by: :prepend
+  # broadcasts_to ->(quote) { [company, "quotes"] }, inserts_by: :prepend
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,11 +10,35 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2024_01_02_160858) do
-  create_table "quotes", charset: "utf8mb4", force: :cascade do |t|
+ActiveRecord::Schema[7.1].define(version: 2024_01_09_065538) do
+  create_table "companies", charset: "utf8mb4", force: :cascade do |t|
     t.string "name", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
 
+  create_table "quotes", charset: "utf8mb4", force: :cascade do |t|
+    t.string "name", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.bigint "company_id", null: false
+    t.index ["company_id"], name: "index_quotes_on_company_id"
+  end
+
+  create_table "users", charset: "utf8mb4", force: :cascade do |t|
+    t.string "email", default: "", null: false
+    t.string "encrypted_password", default: "", null: false
+    t.string "reset_password_token"
+    t.datetime "reset_password_sent_at"
+    t.datetime "remember_created_at"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.bigint "company_id", null: false
+    t.index ["company_id"], name: "index_users_on_company_id"
+    t.index ["email"], name: "index_users_on_email", unique: true
+    t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
+  end
+
+  add_foreign_key "quotes", "companies"
+  add_foreign_key "users", "companies"
 end
